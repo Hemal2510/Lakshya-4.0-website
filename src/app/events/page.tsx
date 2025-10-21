1e1152ae734a529bb6bb138d404be80d762e890c
Hemal2510 <hemal251025@gmail.com>
2025-10-21 12:40:39 +0530

    created dummy pages

---
+++ src/app/events/page.tsx
@@ -0,0 +1,8 @@
+export default function EventsPage() {
+    return (
+        <main className="flex flex-col items-center justify-center min-h-screen">
+            <h1 className="text-4xl font-bold mb-4">Events Page</h1>
+            <p>This is the events page.</p>
+        </main>
+    );
+}
