57fd9de753e12ec17b8e7c01f8608243a3719b1b
Hemal2510 <hemal251025@gmail.com>
2025-12-12 22:47:18 +0530

    added sponsors and typewritter effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import IntroHero from "@/components/Intro";
 import AboutSection from "@/components/AboutSection";
 import GallerySection from "@/components/GallerySection";
+import SponsorsSection from "@/components/SponsorSection";
 export default function LandingPage() {
     return (
         <>
             <IntroHero />
             <AboutSection />
+            <SponsorsSection />
             <GallerySection />
         </>
     );
