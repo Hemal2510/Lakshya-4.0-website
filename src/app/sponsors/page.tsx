a261b54e821a6e5ae66c840b6e69fd3a00153dea
MuskanShukla <shuklamuskan979@gmail.com>
2026-01-06 20:57:30 +0530

    Updated sponsors page

--- src/app/sponsors/page.tsx
+++ src/app/sponsors/page.tsx
@@ -2,16 +2,32 @@
 
 "use client";
 
-import PendulumTextReveal from "@/components/Pendulum";
-import { motion, useMotionValue, useTransform } from "framer-motion";
+// import PendulumTextReveal from "@/components/Pendulum";
+// import { motion, useMotionValue, useTransform } from "framer-motion";
 
-export default function SponsorsPage() {
-  return (
-    <main className="relative min-h-screen w-full overflow-x-hidden overflow-y-auto">
+// export default function SponsorsPage() {
+//   return (
+//     <main className="relative min-h-screen w-full overflow-x-hidden overflow-y-auto">
 
  
-      <PendulumTextReveal/>
-    </main>
-  );
+//       <PendulumTextReveal/>
+//     </main>
+//   );
+// }
+
+
+
+import Sponsorspage from "@/components/SpotlightGlow"
+
+export default function SponsorsPage() {
+    return (
+        <main className="flex flex-col items-center justify-center min-h-screen">
+         
+        <Sponsorspage/>
+
+        </main>
+    );
 }
+
+
 
